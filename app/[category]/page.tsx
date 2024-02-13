df37fe329d15378b3ff43c82f1724eae27ebdd65
Ernesto-Tz <ivan.tzompantzi96@gmail.com>
2024-02-13 14:44:57 +0100

    fix: :lipstick: Fix page height styling

--- app/[category]/page.tsx
+++ app/[category]/page.tsx
@@ -27,14 +27,14 @@ export default async function CategoryPage({
   const data: simplifiedProduct[] = await getData(params.category);
 
   return (
-    <div className="bg-white ">
+    <div className="bg-white h-screen">
       <div className="mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
         <div className="flex justify-between items-center">
           <h2 className="text-2xl font-bold tracking-tight text-gray-900">
             Our Products for {params.category}
           </h2>
         </div>
-        <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+        <div className="mt-12 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
           {data.map((product) => (
             <div key={product._id} className="group relative">
               <div className="aspect-square w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:h-80">
