d056d82c0c249cbbbc0b6a5fbff76a450f0bade2
Ernesto-Tz <ivan.tzompantzi96@gmail.com>
2024-02-20 16:31:18 +0100

    feat: Use ProductCard component

--- app/[category]/page.tsx
+++ app/[category]/page.tsx
@@ -1,7 +1,6 @@
-import Link from "next/link";
 import { simplifiedProduct } from "../interface";
 import { client } from "../lib/sanity";
-import Image from "next/image";
+import ProductCard from "../component/ProductCard";
 
 async function getData(category: string) {
   const query = `*[_type == 'product' && category->name == '${category}' ]{
@@ -36,32 +35,15 @@ export default async function CategoryPage({
         </div>
         <div className="mt-12 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
           {data.map((product) => (
-            <div key={product._id} className="group relative">
-              <div className="aspect-square w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:h-80">
-                <Image
-                  src={product.imageUrl}
-                  alt="Product Image"
-                  className="w-full h-full object-cover object-center lg:h-full lg:w-full"
-                  width={300}
-                  height={300}
-                />
-              </div>
-              <div className="mt-4 flex justify-between">
-                <div>
-                  <h3 className="text-sm text-gray-700">
-                    <Link href={`/product/${product.slug}`}>
-                      {product.name}
-                    </Link>
-                  </h3>
-                  <p className="mt-1 text-sm text-gray-500">
-                    {product.categoryName}
-                  </p>
-                </div>
-                <p className="text-sm font-medium text-gray-500">
-                  ${product.price}
-                </p>
-              </div>
-            </div>
+            <ProductCard
+            key={product._id}
+            _id={product._id}
+            name={product.name}
+            imageUrl={product.imageUrl}
+            slug={product.slug}
+            price={product.price}
+            categoryName={product.categoryName}
+          />
           ))}
         </div>
       </div>
--- app/component/Newest.tsx
+++ app/component/Newest.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import { simplifiedProduct } from "../interface";
 import { client } from "../lib/sanity";
 import { ArrowRight } from "lucide-react";
-import Image from "next/image";
+import ProductCard from "./ProductCard";
 
 async function getData() {
   const query = `*[_type == 'product'][0...4] | order(_createdAt desc){
@@ -29,7 +29,10 @@ export default async function Newest() {
           <h2 className="text-2xl font-bold tracking-tight text-gray-900">
             Our newest product
           </h2>
-          <Link href="/all" className="text-primary flex items-center gap-x-1">
+          <Link
+            href="/All-Products"
+            className="text-primary flex items-center gap-x-1"
+          >
             See All{" "}
             <span>
               <ArrowRight />
@@ -38,32 +41,15 @@ export default async function Newest() {
         </div>
         <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
           {data.map((product) => (
-            <div key={product._id} className="group relative">
-              <div className="aspect-square w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:h-80">
-                <Image
-                  src={product.imageUrl}
-                  alt="Product Image"
-                  className="w-full h-full object-cover object-center lg:h-full lg:w-full"
-                  width={300}
-                  height={300}
-                />
-              </div>
-              <div className="mt-4 flex justify-between">
-                <div>
-                  <h3 className="text-sm text-gray-700">
-                    <Link href={`/product/${product.slug}`}>
-                      {product.name}
-                    </Link>
-                  </h3>
-                  <p className="mt-1 text-sm text-gray-500">
-                    {product.categoryName}
-                  </p>
-                </div>
-                <p className="text-sm font-medium text-gray-500">
-                  ${product.price}
-                </p>
-              </div>
-            </div>
+            <ProductCard
+              key={product._id}
+              _id={product._id}
+              name={product.name}
+              imageUrl={product.imageUrl}
+              slug={product.slug}
+              price={product.price}
+              categoryName={product.categoryName}
+            />
           ))}
         </div>
       </div>
